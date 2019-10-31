941f848d6af6236971b5045f0400a9790e076767
julien lancaster <julien.lancaster@clio.com>
2019-10-31 11:12:58 -0600

    add order object

---
+++ app/text_processing/Order.py
@@ -0,0 +1,29 @@
+class Order:
+
+    def __init__(self):
+        self.state = "greet"
+        self.order = []
+        self.greet()
+
+    def input(self, input):
+        return
+
+
+    def greet(self):
+        print("Well Met")
+
+    def add_item_to_order(self):
+        return
+
+    def add_modification_to_item(self):
+        return
+
+    def confirm_order(self):
+        print(order_string)
+
+    def order_string(self):
+        return
+
+
+
+Order()
