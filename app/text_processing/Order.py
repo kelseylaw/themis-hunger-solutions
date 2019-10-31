ab63c890e1cca597598544162140d567d5f06c67
julien lancaster <julien.lancaster@clio.com>
2019-10-31 12:11:02 -0600

    expand order

--- app/text_processing/Order.py
+++ app/text_processing/Order.py
@@ -6,24 +6,57 @@ class Order:
         self.greet()
 
     def input(self, input):
-        return
+        if self.state == "greet":
+            self.state = "menu_item"
+        elif self.state == "menu_item":
+            self.add_item_to_order(input)
+        elif self.state == "additions":
+            self.add_to_item(input)
+        elif self.state == "removals":
+            self.remove_from_item(input)
+        elif self.state == "is that all":
+            self.order_complete(input)
+        else:
+            self.get_manager
 
 
     def greet(self):
-        print("Well Met")
+        print("Welcome to Themis-- Where customer success comes first. What can I get for you today?")
 
-    def add_item_to_order(self):
+    def prompt_for_menu_item(self):
+        print("anything else for you today?")
+
+    def prompt_for_addition(self):
+        print("anything extra on that?")
+
+    def prompt_for_addition(self):
+        print("need anything removed from that?")
+
+    def prompt_for_end_order(self):
+        print("is that everything?")
+
+    def farewell(self):
+        print("Thank you for choosing Themis hunger solutions, stay fit and have fun")
+
+
+    def add_item_to_order(self, input):
         return
 
-    def add_modification_to_item(self):
+    def add_to_item(self, input):
         return
+
+    def remove_from_item(self, input):
+        return
+
+    def order_complete(self, input):
 
     def confirm_order(self):
-        print(order_string)
+        print(self.order_string)
 
     def order_string(self):
         return
 
-
+    def get_manager(self):
+        print("Let me get my manager to help you")
 
 Order()
